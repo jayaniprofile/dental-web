1041f4dcbe02268105773040d934e5f89087d1f5
jayaniprofile <jayanichamodapathirana@gmail.com>
2024-04-30 13:24:06 +0530

    update footer

--- dental-frontend/src/Components/Footer.jsx
+++ dental-frontend/src/Components/Footer.jsx
@@ -6,6 +6,7 @@ import inster from "../assets/images/inster.png";
 import linkedin from "../assets/images/Linkedin.png";
 import youtube from "../assets/images/Youtube.png";
 import logo from "../assets/images/logoo.png";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
@@ -17,15 +18,24 @@ const Footer = () => {
           </div>
           <div className="footer-column3">
             <h3>NAVIGATE</h3>
-            <p>Home</p>
-            <p>About</p>
-            <p>Services</p>
-            <p>Contact</p>
+            <Link to="/" className="link-without-underline1">
+              <p>Home</p>{" "}
+            </Link>
+            <Link to="/about" className="link-without-underline1">
+              <p>About</p>
+            </Link>
+            <Link to="/setvices" className="link-without-underline1">
+              <p>Services</p>
+            </Link>
+            <Link to="/contact" className="link-without-underline1">
+              <p>Contact</p>
+            </Link>
           </div>
           <div className="footer-column1">
             <h3>CONNECT</h3>
             <p>Email: smiledental@gmail.com</p>
             <p>Phone: 071-012-1234</p>
+            <p>Address: Church Rd,Ratnapura</p>
           </div>
           <div className="footer-column">
             <h3>SOCIALIZE</h3>
